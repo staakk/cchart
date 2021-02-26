2e0b18c7aaf482a07f5d3428c1ae639861dc8ada
Stanislaw Kabacinski <kabacinski.s@gmail.com>
2021-02-26 22:38:55 +0100

    dev: add axis test

---
+++ viewtest/src/androidTest/java/io/github/staakk/cchart/viewtest/AxisTest.kt
@@ -0,0 +1,74 @@
+package io.github.staakk.cchart.viewtest
+
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.unit.dp
+import androidx.test.rule.GrantPermissionRule
+import com.karumi.shot.ScreenshotTest
+import io.github.staakk.cchart.Chart
+import io.github.staakk.cchart.axis.HorizontalAxisLocation
+import io.github.staakk.cchart.axis.VerticalAxisLocation
+import io.github.staakk.cchart.axis.horizontalAxisRenderer
+import io.github.staakk.cchart.axis.verticalAxisRenderer
+import io.github.staakk.cchart.data.DataBounds
+import io.github.staakk.cchart.data.pointOf
+import io.github.staakk.cchart.data.seriesOf
+import io.github.staakk.cchart.renderer.lineRenderer
+import org.junit.Rule
+import org.junit.Test
+
+class AxisTest : ScreenshotTest {
+
+    @Rule
+    @JvmField
+    var permission: GrantPermissionRule = GrantPermissionRule.grant(
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    @Rule
+    @JvmField
+    var composeRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun axisTest() {
+        composeRule.setContent {
+            Chart(
+                modifier = Modifier
+                    .aspectRatio(1f, false)
+                    .padding(16.dp),
+                bounds = DataBounds(0f, 10f, 0f, 5f)
+            ) {
+                series(
+                    seriesOf(
+                        "Data",
+                        pointOf(0f, 1f),
+                        pointOf(2f, 1.5f),
+                        pointOf(3f, 4f),
+                        pointOf(4f, 3.5f),
+                        pointOf(5f, 2f),
+                        pointOf(6f, 1.3f),
+                        pointOf(7f, 4f),
+                        pointOf(8f, 4.5f),
+                        pointOf(9f, 4.7f),
+                    ),
+                    renderer = lineRenderer(brush = SolidColor(Color.Blue))
+                )
+
+                verticalAxis(verticalAxisRenderer())
+
+                verticalAxis(verticalAxisRenderer(location = VerticalAxisLocation.RIGHT))
+
+                horizontalAxis(horizontalAxisRenderer())
+
+                horizontalAxis(horizontalAxisRenderer(location = HorizontalAxisLocation.TOP))
+            }
+        }
+
+        compareScreenshot(composeRule)
+    }
+
+}
